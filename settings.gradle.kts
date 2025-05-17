007c4582f83de4724a8329439b6e3d205a120628
jnasser513 <00245818@uca.edu.sv>
2025-05-17 01:29:31 -0600

    chore: initialize base modules structure

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PokedexApp"
+include(":app")
+include(":pokemon:data")
+include(":pokemon:domain")
+include(":pokemon:presentation")
+include(":pokemon:network")
+include(":core:data")
+include(":core:domain")
+include(":core:presentation:designsystem")
+include(":core:presentation:ui")
